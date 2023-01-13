90ffc2c19f1a33ee6e5014b9b131888d418a6e5e
Артур Лебедев <IinfernalI95@gmail.com>
2023-01-13 19:40:10 +0200

    Initial Commit

---
+++ TableViewHW7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewHW7
+//
+//  Created by Artur on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
